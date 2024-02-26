23dc9f14d90284688b6e2044d760375992f21ed7
jzucko <138820816+jzucko@users.noreply.github.com>
2024-02-26 15:48:18 +0100

    update no1

--- Beginner queries.sql
+++ Beginner queries.sql
@@ -30,3 +30,39 @@ LIMIT 10;
 SELECT title, rating, length 
 FROM film
 WHERE rating LIKE 'P%';
+
+SELECT first_name, last_name FROM actor;
+
+SELECT upper (concat first_name, ' ', last_name) AS actor_name FROM actor;
+
+SELECT actor_id, first_name, last_name FROM actor WHERE first_name LIKE 'Joe';
+
+SELECT first_name, last_name FROM actor WHERE last_name LIKE '%gen%';
+
+SELECT first_name, last_name FROM actor WHERE last_name LIKE '%LI%' ORDER BY last_name, first_name;
+
+SELECT country_id, country FROM country WHERE country IN ('Afghanistan', 'Bangladesh', 'China');
+
+
+ALTER TABLE actor ADD COLUMN middle_name varchar(30) AFTER first_name;
+SELECT * FROM actor;
+
+ALTER TABLE actor MODIFY COLUMN middle_name blob;
+SELECT * FROM actor;
+
+ALTER TABLE actor DROP COLUMN middle_name;
+SELECT * FROM actor;
+/*When you use DROP COLUMN, you are permanently removing the specified column from the table's structure. This means that all data in that 
+column will be deleted, and the column itself will no longer exist in the table.
+On the other hand, DELETE is used to remove rows from a table based on a condition, but it does not remove the structure of the column itself.*/
+
+--https://lyniguez.github.io/SQL-Example-Work/ 4a
+
+
+
+
+
+
+
+
+
